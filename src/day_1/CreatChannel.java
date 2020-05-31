65a07274bd9ba57179955c431210451e2dfab3dc
Osman OLGUN <osmanolgun.de@gmail.com>
2020-05-31 09:07:20 +0200

    Update from Intellij

--- src/day_1/CreatChannel.java
+++ src/day_1/CreatChannel.java
@@ -1,7 +1,15 @@
 package day_1;
 
+import java.util.Scanner;
+
 public class CreatChannel {
     public static void main(String[] args) {
         System.out.println("User is able to create channel");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter your name : ");
+        String name = scan.nextLine();
+
+        for(int i = 1; i<=10; i++) System.out.println("name = " + name);
+        //Lines up here are written from IntelliJ
     }
 }
