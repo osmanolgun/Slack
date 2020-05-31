0012e07c7ceb9886b2c983d12470c1858f2df81e
osmanolgun <62035481+osmanolgun@users.noreply.github.com>
2020-05-31 09:08:17 +0200

    Update CreatChannel.java

--- src/day_1/CreatChannel.java
+++ src/day_1/CreatChannel.java
@@ -3,5 +3,14 @@ package day_1;
 public class CreatChannel {
     public static void main(String[] args) {
         System.out.println("User is able to create channel");
+
+        
+        System.out.println("Osman Olgun");
+        System.out.println("Osman Olgun");
+        System.out.println("Osman Olgun");
+        System.out.println("Osman Olgun");
+        System.out.println("Osman Olgun");
+        System.out.println("Osman Olgun");
+        System.out.println("Osman Olgun");
     }
 }
